96e9ce21dd4ad4aab7777d6d70dc274e7ea1354a
DiegoFernandoSivilaRamirez <diego.dfsr45@gmail.com>
2025-09-08 16:55:36 -0400

    task 2

--- WriteClasses/Classes/TestCase.cs
+++ WriteClasses/Classes/TestCase.cs
@@ -1,10 +1,18 @@
 namespace Classes;
 
-public class TestCase(string title, Func<Task> action, string? priority = null)
+public enum TestPriority
+{
+  P1, // Critical
+  P2, // High
+  P3, // Medium
+  P4  // Low
+}
+
+public class TestCase(string title, Func<Task> action, TestPriority? priority = null)
 {
   // Caracteristics
   public const int MaxTitleLength = 120;
-  private const string DefaultPriority = "P3";
+  private const TestPriority DefaultPriority = TestPriority.P3;
 
   private string _title = title;
 
@@ -25,18 +33,24 @@ public class TestCase(string title, Func<Task> action, string? priority = null)
     }
   }
   // TODO: Change the priority to an enum: P1, P2, P3, P4. Add validations if necessary.
-  public string Priority { get; set; } = priority ?? DefaultPriority;
+  public TestPriority Priority { get; set; } = priority ?? DefaultPriority;
   // TODO: Change the status to an enum: NotRun, Passed, Failed
   public string Status { get; private set; } = "NotRun";
   public string? FailureReason { get; private set; }
   public Func<Task> Action { get; } = action ?? throw new ArgumentNullException(nameof(action));
 
   // Behavior
-  public bool IsHighPriority => Priority is "P1";
+  public bool IsHighPriority => Priority is TestPriority.P1;
 
   public void MarkPassed() => Status = "Passed";
-  // TODO: Add validation for reason.
-  public void MarkFailed(string reason) => (Status, FailureReason) = ("Failed", reason);
+  public void MarkFailed(string? reason) 
+  {
+    if (reason != null && reason.Length > 150)
+    {
+      reason = reason.Substring(0, 150); 
+    }
+    (Status, FailureReason) = ("Failed", reason);
+  }
 
   public async Task ExecuteAsync()
   {
