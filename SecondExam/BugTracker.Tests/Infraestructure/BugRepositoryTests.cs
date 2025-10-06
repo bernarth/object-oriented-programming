b3629e71b9de8493a845773f48715ea8b3ca1505
Joanna Yujra <joanna.yujra@outlook.es>
2025-10-06 19:56:05 -0400

    Add unit tests

--- SecondExam/BugTracker.Tests/Infraestructure/BugRepositoryTests.cs
+++ SecondExam/BugTracker.Tests/Infraestructure/BugRepositoryTests.cs
@@ -1,3 +1,9 @@
+using AutoFixture;
+using BugTracker.Infrastructure;
+using BugTracker.Domain;
+
+namespace BugTracker.Tests.Infrastructure;
+
 public class BugRepositoryTests
 {
 
@@ -7,12 +13,9 @@ public class BugRepositoryTests
   public void Add_OneBug_ShouldCreateOneBug()
   {
     string title = _fixture.Create<string>().Substring(0, 12);
-    string severity = _fixture.Create<string>().Substring(0, 12);
-    var bug = new BugRepositoryTests
-    {
-      Title = title,
-      Severity = severity
-    };
+    Severity severity = Severity.Medium;
+    int id = 10;
+    var bug = new Bug(id, title, severity);
     var bugRepository = new BugRepository();
 
     Bug result = bugRepository.Add(bug);
@@ -20,7 +23,51 @@ public class BugRepositoryTests
     Assert.Equal(bug.Title, result.Title);
     Assert.Equal(bug.Severity, result.Severity);
     Assert.Equal(1, result.Id);
+  }
+  [Fact]
+  public void GetById_GetFirstId_ShouldReturnFirst()
+  {
+    string title = _fixture.Create<string>().Substring(0, 12);
+    Severity severity = Severity.Medium;
+    int id = 10;
+    var bug = new Bug(id, title, severity);
+    var bugRepository = new BugRepository();
+    bugRepository.Add(bug);
 
+    Bug? result = bugRepository.GetById(1);
 
+    Assert.Equal(bug.Title, result?.Title);
+    Assert.Equal(bug.Severity, result?.Severity);
+    Assert.Equal(1, result?.Id);
+  }
+
+  [Fact]
+  public void GetById_NonExistentId_ShouldReturnNull()
+  {
+    string title = _fixture.Create<string>().Substring(0, 12);
+    Severity severity = Severity.Medium;
+    int id = 10;
+    var bug = new Bug(id, title, severity);
+    var bugRepository = new BugRepository();
+    bugRepository.Add(bug);
+
+    Bug? result = bugRepository.GetById(2);
+
+    Assert.Null(result);
+  }
+  
+  [Fact]
+  public void GetAll_ShouldReturnList()
+  {
+    string title = _fixture.Create<string>().Substring(0, 12);
+    Severity severity = Severity.Medium;
+    int id = 10;
+    var bug = new Bug(id,title,severity);
+    var bugRepository = new BugRepository();
+    bugRepository.Add(bug);
+
+    List<Bug> result = bugRepository.GetAll();
+
+    Assert.IsType<List<Bug>>(result);
   }
 }
